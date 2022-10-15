7066cbfca0a82316a50ed5cec5724b23c5dfba31
petfelt <peteraminfelton@gmail.com>
2022-10-15 09:31:15 -0700

    Plasma grenade fix, syntax changes, different leader script traits with more players

--- frozen-throne/frozen-throne.vb
+++ frozen-throne/frozen-throne.vb
@@ -22,11 +22,12 @@ declare player.number[2] with network priority low
 declare player.number[3] with network priority low
 declare player.number[4] with network priority low
 declare player.number[5] with network priority local
+declare player.number[6] with network priority low
 declare player.object[0] with network priority low
 declare player.timer[0] = 5
 declare player.timer[1] = 5
 declare player.timer[2] = 15
-declare player.timer[3] = 6
+declare player.timer[3] = 5
 declare object.timer[0] = 1
 
 if game.teams_enabled == 1 then 
@@ -48,20 +49,25 @@ for each player do
    if not current_player.is_elite() then 
       current_player.set_loadout_palette(spartan_tier_1)
    end
+   global.number[11] = 0
+end
+
+for each player do
+   global.number[11] += 1
 end
 
 for each player do
    if game.score_to_win != 0 and game.teams_enabled == 1 then 
-      current_player.set_round_card_title("Kill players on the enemy team.\r\n%n points to win.", game.score_to_win)
+      current_player.set_round_card_title("Kill, headshot, and stick enemies to rule.\nRule the Throne to earn more points.\n%n points to win.", game.score_to_win)
    end
    if game.score_to_win != 0 and game.teams_enabled == 0 then 
-      current_player.set_round_card_title("Score points by killing other players.\r\n%n points to win.", game.score_to_win)
+      current_player.set_round_card_title("Get kills, headshots, and sticks to rule.\nRule the Throne to earn more points.\n%n points to win.", game.score_to_win)
    end
    if game.score_to_win == 0 and game.teams_enabled == 1 then 
-      current_player.set_round_card_title("Kill players on the enemy team.")
+      current_player.set_round_card_title("Kill, headshot, and stick enemies \nto take the Throne.\nRule the Throne to earn more points.")
    end
    if game.score_to_win == 0 and game.teams_enabled == 0 then 
-      current_player.set_round_card_title("Score points by killing other players.")
+      current_player.set_round_card_title("Get kills, headshots, and sticks\nto take the Throne.\nRule the Throne to earn more points.")
    end
 end
 
@@ -88,13 +94,8 @@ end
 
 for each player do
    current_player.timer[0].set_rate(-100%)
-   current_player.timer[3].set_rate(-100%)
    if current_player.number[1] == 0 and current_player.timer[0].is_zero() then 
-      send_incident(custom_game_start, current_player, no_player)
-      current_player.number[1] = 1
-   end
-   if current_player.number[1] == 1 and current_player.timer[3].is_zero() then 
-      game.show_message_to(current_player, announce_vip, "VIP")
+      game.show_message_to(current_player, announce_vip, "Frozen Throne\n\nKill enemy Kings when you can!")
       current_player.number[1] = 3
    end
    if current_player.timer[3].is_zero() and current_player.number[5] == 1 then 
@@ -104,17 +105,15 @@ for each player do
    end
 end
 
-do
+for each player do
    global.number[4] = game.round_timer
    global.number[4] %= 10
-   if global.number[4] == 0 and global.number[5] == 0 then
-      for each player do
-         current_player.plasma_grenades += 1
-      end
-      global.number[5] = 1
+   if global.number[4] == 0 and current_player.number[6] == 0 then 
+      current_player.plasma_grenades += 1
+      current_player.number[6] = 1
    end
-   if global.number[4] != 0 then
-      global.number[5] = 0
+   if global.number[4] != 0 and current_player.number[6] != 0 then 
+      current_player.number[6] = 0
    end
 end
 
@@ -161,10 +160,15 @@ end
 for each player do
    global.player[0] = current_player
    if current_player.number[0] == 1 and current_player.number[4] == 1 then 
-      game.play_sound_for(current_player, announce_koth_controlled, true)
+      game.play_sound_for(current_player, announce_koth_controlled, false)
       for each player do
          if current_player != global.player[0] then 
-            game.show_message_to(current_player, announce_koth_moved, "%s will be the new Frozen King!", global.player[0])
+            if current_player.team == global.player[0].team then 
+               game.show_message_to(current_player, announce_koth_controlled, "%s will be the new Frozen King!", global.player[0])
+            end
+            if current_player.team != global.player[0].team then 
+               game.show_message_to(current_player, announce_koth_moved, "%s will be the new Frozen King!", global.player[0])
+            end
          end
       end
       current_player.number[4] = 2
@@ -173,12 +177,23 @@ for each player do
       script_widget[0].set_text("You will be the new Frozen King!")
       script_widget[1].set_text("Respawning in: %n", hud_player.timer[1])
       script_widget[0].set_visibility(current_player, true)
-      script_widget[1].set_visibility(current_player, true)
-      current_player.apply_traits(script_traits[0])
+      global.object[0] = no_object
+      global.object[0] = current_player.biped
+      if global.object[0] != no_object then 
+         script_widget[1].set_visibility(current_player, true)
+      end
+      if global.object[0] == no_object then 
+         script_widget[1].set_visibility(current_player, false)
+      end
+      if global.number[11] <= 11 then 
+         current_player.apply_traits(script_traits[0])
+      end
+      if global.number[11] >= 12 then 
+         current_player.apply_traits(script_traits[1])
+      end
       if current_player.timer[1].is_zero() then 
          script_widget[0].set_visibility(current_player, false)
          script_widget[1].set_visibility(current_player, false)
-         current_player.apply_traits(script_traits[0])
          current_player.biped.kill(false)
          current_player.number[4] = 3
       end
@@ -187,7 +202,12 @@ end
 
 for each player do
    if current_player.number[0] == 1 and current_player.timer[1].is_zero() then 
-      current_player.apply_traits(script_traits[0])
+      if global.number[11] <= 11 then 
+         current_player.apply_traits(script_traits[0])
+      end
+      if global.number[11] >= 12 then 
+         current_player.apply_traits(script_traits[1])
+      end
       if current_player.number[4] == 3 and current_player.timer[2].is_zero() then 
          global.player[0] = current_player
          current_player.biped.set_waypoint_icon(vip)
@@ -196,22 +216,27 @@ for each player do
          global.player[0] = current_player
          script_widget[3].set_text("New Frozen Throne")
          for each player do
-            script_widget[3].set_visibility(current_player, true)
-            game.play_sound_for(current_player, announce_vip_new, true)
+            game.play_sound_for(current_player, announce_vip_new, false)
             if current_player != global.player[0] then 
-               game.show_message_to(current_player, none, "King %s has chosen a throne", global.player[0])
+               script_widget[3].set_visibility(current_player, true)
+               if current_player.team == global.player[0].team then 
+                  game.show_message_to(current_player, none, "Protect King %s!", global.player[0])
+               end
+               if current_player.team != global.player[0].team then 
+                  game.show_message_to(current_player, none, "Kill King %s!", global.player[0])
+               end
             end
             if current_player == global.player[0] then 
                game.show_message_to(current_player, none, "You have chosen a throne")
             end
-            current_player.timer[3] = 5
             current_player.timer[3].reset()
+            current_player.timer[3].set_rate(-100%)
             current_player.number[5] = 1
          end
          current_player.number[4] = 4
       end
       if current_player.number[4] == 4 then 
-         current_player.apply_traits(script_traits[1])
+         current_player.apply_traits(script_traits[2])
       end
    end
 end
@@ -299,6 +324,9 @@ for each player do
             global.player[0].score += script_option[6]
          end
       end
+      if current_player.number[0] == 1 and current_player.number[4] >= 4 then 
+         current_player.number[4] = 1
+      end
    end
 end
 
@@ -306,7 +334,12 @@ for each player do
    if current_player.killer_type_is(guardians | suicide | kill | betrayal | quit) and not current_player.killer_type_is(kill) and not current_player.killer_type_is(betrayal) then 
       global.number[3] = current_player.try_get_death_damage_type()
       if global.number[3] != 2 then 
-         current_player.score += script_option[3]
+         if current_player.number[0] != 1 then 
+            current_player.score += script_option[3]
+         end
+         if current_player.number[0] == 1 then 
+            current_player.score += script_option[13]
+         end
       end
    end
 end
